3a968f03ca2ca38a271dbac5fc8c553718156253
joulis1derful <joulis1derful@gmail.com>
2017-07-16 18:53:12 +0300

    Implemented filter by countries

--- src/main/java/controller/DisplayController.java
+++ src/main/java/controller/DisplayController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.User;
 import persistence.DAO;
 
 import javax.servlet.ServletException;
@@ -9,14 +10,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "DisplayController", urlPatterns = "/users")
 public class DisplayController extends HttpServlet {
+    private List<User> users;
+    public DisplayController() {
+        super();
+        users = new ArrayList<>();
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String country = request.getParameter("country");
+        users = DAO.getWithCountry(country);
+        response.sendRedirect("/users");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("users", DAO.getUsers());
+        request.setAttribute("filteredbycountry", users);
         request.getRequestDispatcher("users.jsp").forward(request,response);
     }
 }
