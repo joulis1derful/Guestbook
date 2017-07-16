d482d4a65856fa8caf002aa2545d47bf81d72c36
joulis1derful <joulis1derful@gmail.com>
2017-07-16 17:50:53 +0300

    Configured registration process

--- src/main/java/controller/RegisterController.java
+++ src/main/java/controller/RegisterController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import persistence.DAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,15 @@ import java.io.IOException;
 @WebServlet(name = "RegisterController", urlPatterns = "/register")
 public class RegisterController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String fname = request.getParameter("fname");
+        String lname = request.getParameter("lname");
+        String email = request.getParameter("email");
+        String dob = request.getParameter("dob");
+        String tel = request.getParameter("tel");
+        String country = request.getParameter("country");
+        String city = request.getParameter("city");
+        DAO.insertToDb(fname, lname, email, dob, tel, country, city);
+        response.sendRedirect("/guests");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
